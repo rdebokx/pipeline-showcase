14bb244c62f10bcf1c182923f3bfe9faba7dcfa5
Roy de Bokx <info@debokxict.nl>
2018-04-13 15:59:20 +0200

    testing GreeterActor

--- src/test/kotlin/com/rdebokx/pipelineshowcase/actors/GreeterActorTest.kt
+++ src/test/kotlin/com/rdebokx/pipelineshowcase/actors/GreeterActorTest.kt
@@ -5,8 +5,11 @@ import akka.actor.ActorSystem
 import akka.actor.Props
 import akka.testkit.TestActors
 import akka.testkit.TestKit
-import com.nhaarman.mockito_kotlin.mock
+import akka.testkit.TestProbe
+import com.nhaarman.mockito_kotlin.*
+import com.rdebokx.pipelineshowcase.ActorHelper
 import com.rdebokx.pipelineshowcase.Greeting
+import org.mockito.ArgumentMatchers
 import org.springframework.kafka.core.KafkaTemplate
 import kotlin.test.*
 import scala.concurrent.duration.Duration
@@ -16,39 +19,35 @@ import java.util.concurrent.TimeUnit
 
 class GreeterActorTest: TestKit(system) {
 
-
-
-
     @Test
     fun `Greeting forwarded to LogActor and WordActor`(){
-        val logActor = system.actorOf(TestActors.echoActorProps())
-        val prefix = "Test prefix "
+        //Return mock wordActor when created by GreetingActor
         val kafkaMock = mock<KafkaTemplate<String, String>>()
-        val greetingActor = system.actorOf(Props.create(GreeterActor::class.java, prefix, logActor, kafkaMock))
+
+        //Create greetingActor with stubbed LogActor and kafkaMock.
+        val logActor = TestProbe(system)
+        val prefix = "Test prefix "
+        val greetingActor = system.actorOf(Props.create(GreeterActor::class.java, prefix, logActor.ref(), kafkaMock))
+
+        val wordActor = TestProbe(system)
+        system.actorOf(TestActors.forwardActorProps(wordActor.ref()), ActorHelper.WORD_ACTOR_PREFIX + "1")
 
         val testGreeting = Greeting(42, "A test message!")
-
         greetingActor.tell(testGreeting, ActorRef.noSender())
 
-        //TODO: debug
-        expectMsg(FiniteDuration(3, TimeUnit.SECONDS), prefix + testGreeting.content)
-
-
-
-
-        fail("TODO")
-
+        //Test that logActor was called
+        logActor.expectMsg(FiniteDuration(3, TimeUnit.SECONDS), prefix + testGreeting.content)
+        //Test that wordActor was created and called
+        wordActor.expectMsg(FiniteDuration(3, TimeUnit.SECONDS), testGreeting)
     }
 
     companion object {
 
-        val system = ActorSystem.create()
+        val system = ActorSystem.create("GreetingActorTestSystem")
 
         @AfterTest
         fun shutDown() {
             TestKit.shutdownActorSystem(system, Duration.create("3 seconds"), false)
         }
-
-
     }
 }
